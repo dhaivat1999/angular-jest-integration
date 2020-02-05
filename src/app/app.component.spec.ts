31d4426227e98210d757c2ac1e1cc9240cda5851
dhaivat1999 <dhaivat1999@gmail.com>
2020-02-05 16:30:55 +0530

    setup complete

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,12 +18,20 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+    console.log("App Created")
   });
+  test(`the title is 'my-awesome-app'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('my-awesome-app');
+    console.log("This is test block")
+  }));
 
   it(`should have as title 'my-awesome-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('my-awesome-app');
+    console.log("Title allocated")
   });
 
   it('should render title', () => {
@@ -31,5 +39,6 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('my-awesome-app app is running!');
+    console.log("content loaded")
   });
 });
