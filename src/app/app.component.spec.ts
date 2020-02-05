7751fbc6cb3f338ae2f9e6a13c95a8da4c44b1a4
dhaivat1999 <dhaivat1999@gmail.com>
2020-02-05 16:52:08 +0530

    step2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,32 +13,16 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-    console.log("App Created")
-  });
   test(`the title is 'my-awesome-app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('my-awesome-app');
     console.log("This is test block")
   }));
-
-  it(`should have as title 'my-awesome-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-awesome-app');
-    console.log("Title allocated")
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-awesome-app app is running!');
-    console.log("content loaded")
-  });
+  // test("this is to check if the app has the variable or not?", async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.var1).toBeTruthy();
+  //   console.log("This is test block")
+  // }));
 });
